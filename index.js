add2fd382837074a6c057821c43c7525892a9b6d
github username <github email address>
2021-06-13 16:29:13 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
+  const input = document.getElementById('input');
 
 function clickAlert() {
   alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert); 
+input.addEventListener('click', clickAlert);
 }
